dee77f7c661db0710e5aa2b8e2ad7cde58ed4b90
Ilya Korolev <ilya_korolev@zoho.eu>
2020-05-03 14:34:19 +0300

    added vector math

---
+++ src/MathExtra/Vector2d.cs
@@ -0,0 +1,17 @@
+namespace RayTracingEngine.MathExtra
+{
+   public struct Vector2d
+   {
+      public double X { get; set; }
+      public double Y { get; set; }
+
+      public Vector2d(double x, double y)
+      {
+         X = x;
+         Y = y;
+      }
+
+      public static implicit operator Vector3d(Vector2d self)
+          => new Vector3d(self.X, self.Y, 0d);
+   }
+}
---
+++ src/MathExtra/Vector3d.cs
@@ -0,0 +1,69 @@
+using System;
+
+namespace RayTracingEngine.MathExtra
+{
+   public struct Vector3d
+   {
+      public double X { get; set; }
+      public double Y { get; set; }
+      public double Z { get; set; }
+
+      public double Length { get => Math.Sqrt(this * this); }
+
+      public Vector3d(double x, double y, double z)
+      {
+         X = x;
+         Y = y;
+         Z = z;
+      }
+
+      // vector-vector
+      public static Vector3d operator +(Vector3d left, Vector3d right)
+          => new Vector3d(left.X + right.X, left.Y + right.Y, left.Z + right.Z);
+
+      public static Vector3d operator -(Vector3d left, Vector3d right)
+          => new Vector3d(left.X - right.X, left.Y - right.Y, left.Z - right.Z);
+
+      public static double operator *(Vector3d left, Vector3d right)
+          => left.X * right.X + left.Y * right.Y + left.Z * right.Z;
+
+
+      // vector-scalar
+      public static Vector3d operator +(Vector3d left, double right)
+          => new Vector3d(left.X + right, left.Y + right, left.Z + right);
+
+      public static Vector3d operator +(double left, Vector3d right)
+          => right + left;
+
+
+      public static Vector3d operator -(Vector3d left, double right)
+          => new Vector3d(left.X - right, left.Y - right, left.Z - right);
+
+
+      public static Vector3d operator *(Vector3d left, double right)
+          => new Vector3d(left.X * right, left.Y * right, left.Z * right);
+
+      public static Vector3d operator *(double left, Vector3d right)
+          => right * left;
+
+
+      public static Vector3d operator /(Vector3d left, double right)
+          => left * (1d / right);
+
+
+      // unary operations
+      public static Vector3d operator -(Vector3d self)
+          => new Vector3d(-self.X, -self.Y, -self.Z);
+
+
+      public Vector3d Normalize()
+          => this / Length;
+
+      public Vector3d Reflect(Vector3d normal)
+          => 2d * normal * (normal * this) - this;
+
+
+      public static explicit operator Vector2d(Vector3d self)
+          => new Vector2d(self.X, self.Y);
+   }
+}
